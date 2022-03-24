949dd5c2dce800ac9a5a4e0bc1615ca216913bac
richandersn <rich.anderson@gmail.com>
2022-03-24 17:40:42 -0500

    Added nix/home manager install script.

---
+++ nix/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+source ~/.nix-profile/etc/profile.d/nix.sh
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
+nix-shell '<home-manager>' -A install
